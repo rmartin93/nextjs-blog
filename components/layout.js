6ce9fcc04543b821a39709d33c19820985263f2e
Ryan Martin <ryan@meraki-digital.com>
2023-01-24 12:01:23 -0600

    tweak

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Your Name";
-export const siteTitle = "Dev Test";
+export const siteTitle = "Dev Test 2";
 
 export default function Layout({ children, home }) {
 	return (
